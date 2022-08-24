b477459148f8f234b83ee2c9a9e0f52bd1e9e113
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-08-24 04:42:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+function overwriteBestCustomer(newBestCustomer) {
+    window.bestCustomer = newBestCustomer;
+}
+const leastFavoriteCustomer = 'Becky';
+function changeLeastFavoriteCustomer(newLeastFavoriteCustomer) {
+    leastFavoriteCustomer = newLeastFavoriteCustomer;
+}
